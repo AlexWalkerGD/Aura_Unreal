aca46b07d81638632a24783707bcd592ba37e8eb
Alex Walker <67351804+AlexWalkerGD@users.noreply.github.com>
2024-11-13 00:51:17 +0000

    User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Alex Walker'GD
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Alex Walker'GD
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
