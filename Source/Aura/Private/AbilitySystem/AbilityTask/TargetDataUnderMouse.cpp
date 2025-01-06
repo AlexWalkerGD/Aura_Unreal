43f1db67078510fdaf8c293e726cc7b49b639cc9
Alex Walker <67351804+AlexWalkerGD@users.noreply.github.com>
2025-01-06 23:18:41 +0000

    Send mouse cursor data

--- Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTask/TargetDataUnderMouse.cpp
@@ -2,6 +2,8 @@
 
 
 #include "AbilitySystem/AbilityTask/TargetDataUnderMouse.h"
+#include "AbilitySystemComponent.h"
+#include "Android/AndroidInputInterface.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -11,5 +13,34 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)
+	{
+		SendMouseCursorData();
+	}
+	else
+	{
+		
+	}
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	FScopedPredictionWindow ScopePrediction(AbilitySystemComponent.Get());
 	
+	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	FHitResult CursorHit;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	DataHandle.Add(Data);
+	
+	AbilitySystemComponent->ServerSetReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey(),DataHandle, FGameplayTag(),AbilitySystemComponent->ScopedPredictionKey);
+
+	if(ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
