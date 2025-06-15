45565b4a3dc8d350ae976938c639c100254fed3c
Alex Walker <67351804+AlexWalkerGD@users.noreply.github.com>
2025-06-15 01:32:03 +0100

    FireBolt Description

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectilSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectilSpell.cpp
@@ -9,6 +9,25 @@
 #include "Aura/Public/AuraGameplayTags.h"
 #include "Interact/CombatInterface.h"
 
+FString UAuraProjectilSpell::GetDescription(int32 Level)
+{
+	const int32 Damage = DamageTypes[FAuraGameplayTags::Get().Damage_Fire].GetValueAtLevel(Level);
+	if(Level == 1)
+	{
+		return FString::Printf(TEXT("<Title>FIRE BOLT</>\n\n<Default>Launches a bolt of fire, exploding on impact and dealing: </><Damage>%d</><Default>fire damage with a chance to burn</>\n\n<Small>Level:</><Level>%d</>"), Damage, Level);
+	}
+	else
+	{
+		return FString::Printf(TEXT("<Title>FIRE BOLT</>\n\n<Default>Launches %d bolts of fire, exploding on impact and dealing: </><Damage>%d</><Default>fire damage with a chance to burn</>\n\n<Small>Level:</><Level>%d</>"), FMath::Min(Level, NumProjectiles), Damage, Level);
+	}
+}
+
+FString UAuraProjectilSpell::GetNextLevelDescription(int32 Level)
+{
+	const int32 Damage = DamageTypes[FAuraGameplayTags::Get().Damage_Fire].GetValueAtLevel(Level);
+	return FString::Printf(TEXT("<Title>NEXT LEVEL</>\n\n<Default>Launches %d bolts of fire, exploding on impact and dealing: </><Damage>%d</><Default>fire damage with a chance to burn</>\n\n<Small>Level:</><Level>%d</>"), FMath::Min(Level, NumProjectiles), Damage, Level);
+}
+
 void UAuraProjectilSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
                                           const FGameplayEventData* TriggerEventData)
